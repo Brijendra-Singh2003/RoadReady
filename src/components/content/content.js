1f32c358fee3641918ab29d3c6c5df9c131263de
Brijendra-Singh2003 <brijendra0369@gmail.com>
2023-10-07 00:20:15 +0530

    bottom bar added

--- src/components/content/content.js
+++ src/components/content/content.js
@@ -4,27 +4,30 @@ import { BsFillFuelPumpFill } from "react-icons/bs";
 import styles from "./content.module.css"
 import { MdCarRepair } from "react-icons/md";
 import { FaHandsHelping } from "react-icons/fa";
+import { useState } from "react";
+
 
 export default function Content() {
-  const a = 5;
+  const arr = [styles._L,styles._M, styles._R];
+  const [a, setA] = useState(0);
 
   return (
     <>
       <div className={styles.wraper}>
-        <button className={styles.options}>
+        <button onClick={()=>setA(0)} className={styles.options}>
           <BsFillFuelPumpFill className={styles.icon + " " + styles.fuellogo} />
           <h2>FUEL</h2>
         </button>
-        <button className={styles.options}>
+        <button onClick={()=>setA(1)} className={styles.options}>
           <MdCarRepair className={styles.icon} />
           <h2>REPAIR</h2>
         </button>
-        <button className={styles.options}>
+        <button onClick={()=>setA(2)} className={styles.options}>
           <FaHandsHelping className={styles.icon} />
           <h2>REPAIR</h2>
         </button>
       </div>
-      <div className={styles.bar+" _bar"}></div>
+      <div className={styles.bar+" "+arr[a]}></div>
       <div className="content"></div>
     </>
   )
